31e135cf1b3d83e1aa507f91587811115da09cdd
Kwynett Bragado <kwynett.bragado@ibm.com>
2021-06-11 11:21:41 +0800

    TS objects

---
+++ basics.js
@@ -0,0 +1,9 @@
+function add(num1, num2, showResult) {
+    if (showResult)
+        console.log(num1 + num2);
+    return num1 + num2;
+}
+var first = 5;
+var second = 2.8;
+var prints = true;
+add(first, second, prints);
---
+++ basics.ts
@@ -0,0 +1,13 @@
+function add(num1: number, num2: number, showResult: boolean) {
+    if (showResult)
+        console.log(num1 + num2)   
+    return num1 + num2;
+}
+
+
+
+const first = 5;
+const second = 2.8;
+const prints = true;
+
+add(first, second, prints);
