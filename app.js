6bec8b800d38ec0fb5a5ddd905986da4f87e4694
Kwynett Bragado <kwynett.bragado@ibm.com>
2021-06-11 10:46:07 +0800

    typesystem of TS

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Code goes here.");
+function add(num1, num2) {
+    return num1 + num2;
+}
+var first = 5;
+var second = 2.8;
+var result = add(first, second);
+console.log(result);
--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log("Code goes here.")
+function add(num1: number, num2: number) {
+    return num1 + num2;
+}
+
+const first = 5;
+const second = 2.8;
+
+const result = add(first, second);
+console.log(result);
