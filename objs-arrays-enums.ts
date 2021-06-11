c3d17390360c79899dd28c0cf43c059fb170e420
Kwynett Bragado <kwynett.bragado@ibm.com>
2021-06-11 12:02:56 +0800

    Objects, Arrays, Enums

---
+++ objs-arrays-enums.ts
@@ -0,0 +1,11 @@
+enum Skills { HTML5, CSS, JS, TS, PYTHON, JAVA};
+
+const person = {
+    name: "Kwynett Bragado",
+    age: 22,
+    role: "Developer",
+    interest: ["Web Development", "Machine Learning", "Quantum Technology", "App Development"],
+    skills: Skills[5]
+};
+
+console.log(person.skills);
